e92a54ba1702b763b8db52641b6f8286a48af3fc
Zhen Ting, Neo <neozt99@gmail.com>
2024-12-27 16:35:03 +0800

    clean(1014): use clearer initial values - Technically best should start at -math.inf as first score calculated during first iteration

--- python/1014.py
+++ python/1014.py
@@ -6,8 +6,8 @@ class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
         # score = (values[i] + i) + (values[j] - j)
         # we use m to keep track of best values[i] + i seen so far
-        best = values[0]
-        m = values[0]
+        best = -math.inf
+        m = values[0] + 0
 
         for i in range (1, len(values)):
             best = max(best, m + values[i] - i)
