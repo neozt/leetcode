5e7a405c64a5480e405c8780c1cadfee9f113107
Zhen Ting, Neo <neozt99@gmail.com>
2024-12-28 11:25:41 +0800

    3011 - refactor to use in built bit_count function

--- python/3011.py
+++ python/3011.py
@@ -4,12 +4,12 @@ from typing import List
 
 class Solution:
     def canSortArray(self, nums: List[int]) -> bool:
-        current_group = self.countSetBits(nums[0])
+        current_group = nums[0].bit_count()
         current_group_max = nums[0]
         previous_group_max = -math.inf
         for i in range(1, len(nums)):
             current = nums[i]
-            next_group = self.countSetBits(current)
+            next_group = current.bit_count()
             if next_group != current_group:
                 previous_group_max = current_group_max
                 current_group = next_group
@@ -21,14 +21,6 @@ class Solution:
             current_group_max = max(current_group_max, current)
 
         return True
-
-    def countSetBits(self, n):
-        count = 0
-        while n:
-            if n & 1:
-                count += 1
-            n = n >> 1
-        return count
 
 print(Solution().canSortArray([8,4,2,30,15]))
 print(Solution().canSortArray([1,2,3,4,5]))
