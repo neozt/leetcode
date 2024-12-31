7f7136a06a711240d8817670082f3347111ec4ed
Neo Zhen Ting <neozt99@gmail.com>
2024-12-31 21:06:29 +0800

    Optimized 2466

--- 2466.py
+++ 2466.py
@@ -6,9 +6,9 @@ class Solution:
         dp[0] = 1
         for i in range(1, high + 1):
             if i >= zero:
-                dp[i] += dp[i - zero]
+                dp[i] += dp[i - zero] % MOD
             if i >= one:
-                dp[i] += dp[i - one]
+                dp[i] += dp[i - one] % MOD
 
         return sum(dp[i] for i in range(low, high + 1)) % MOD
 
