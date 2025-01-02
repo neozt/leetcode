a18286b5171c78bf3a4696ca40239c77406bbd72
Neo Zhen Ting <neozt99@gmail.com>
2025-01-02 20:44:49 +0800

    Renamed variable

--- python/2559.py
+++ python/2559.py
@@ -4,16 +4,16 @@ from typing import List
 class Solution:
     def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
         n = len(words)
-        dp = [0] * (n + 1) # Let dp[i] = number of words up to i (exclusive) in words array that start and end with a vowel
+        prefix_sum = [0] * (n + 1) # Let prefix_sum[i] = number of words up to i (exclusive) in words array that start and end with a vowel
 
         for i in range(n):
             word = words[i]
             if is_vowel(word[0]) and is_vowel(word[-1]):
-                dp[i + 1] = dp[i] + 1
+                prefix_sum[i + 1] = prefix_sum[i] + 1
             else:
-                dp[i + 1] = dp[i]
+                prefix_sum[i + 1] = prefix_sum[i]
 
-        return [dp[r + 1] - dp[l] for (l, r) in queries]
+        return [prefix_sum[r + 1] - prefix_sum[l] for l, r in queries]
 
 def is_vowel(ch):
     return ch in 'aeiou'
