cb9e157fdd8a3d05b78d83f60243346f2c4cb064
Neo Zhen Ting <neozt99@gmail.com>
2025-01-05 21:46:55 +0800

    Optimized 3163

--- python/3163.py
+++ python/3163.py
@@ -1,13 +1,12 @@
 class Solution:
     def compressedString(self, word: str) -> str:
-        prev_char = None
-        count = 0
+        prev_char = word[0]
+        count = 1
 
         result = []
-        for ch in word:
-            if prev_char is None or ch != prev_char or count == 9:
-                if prev_char is not None:
-                    result.append(str(count) + prev_char)
+        for ch in word[1:]:
+            if ch != prev_char or count == 9:
+                result.append(str(count) + prev_char)
                 prev_char = ch
                 count = 1
             else:
