3c141077c4764ef94c9744004ed493341844918a
Neo Zhen Ting <neozt99@gmail.com>
2025-01-13 20:06:37 +0800

    Solved 3223

---
+++ python/3223.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        count = [0] * 26
+        for ch in s:
+            count[ord(ch) - ord('a')] += 1
+
+        return sum(reduce(n) for n in count)
+
+def reduce(n: int) -> int:
+    if n == 0:
+        return 0
+
+    return 2 if n % 2 == 0 else 1
