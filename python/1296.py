c498c4ec34b97659f2e5697640c42f3c3e74dd77
Neo Zhen Ting <neozt99@gmail.com>
2025-01-14 22:07:02 +0800

    Solved 1296

---
+++ python/1296.py
@@ -0,0 +1,25 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def isPossibleDivide(self, nums: List[int], k: int) -> bool:
+        if len(nums) % k != 0:
+            return False
+
+        freq = Counter(nums)
+        nums_sorted = sorted(freq.keys())
+
+        for n in nums_sorted:
+            while freq[n]:
+                for i in range(n, n + k):
+                    freq[i] -= 1
+                    if freq[i] < 0:
+                        return False
+
+        return True
+
+print(Solution().isPossibleDivide([1,2,3,3,4,4,5,6], 4))
+print(Solution().isPossibleDivide([3,2,1,2,3,4,3,4,5,9,10,11], 3))
+print(Solution().isPossibleDivide([1,2,3,4], 3))
+print(Solution().isPossibleDivide([16,21,26,35], 4))
