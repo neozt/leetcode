8a2216439d96686a9e6a42a42808269220fa6a81
Zhen Ting, Neo <neozt99@gmail.com>
2025-02-02 12:25:20 +0800

    Optimized 1752

--- python/1752.py
+++ python/1752.py
@@ -3,19 +3,14 @@ from typing import List
 
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        for i in range(len(nums)):
-            is_sorted = True
+        reset_count = 0
+        for i in range(0, len(nums)):
+            if nums[i] < nums[(i - 1) % len(nums)]:
+                reset_count += 1
+                if reset_count > 1:
+                    return False
 
-            for j in range(1, len(nums)):
-                idx = (i + j) % len(nums)
-                if nums[idx] < nums[idx - 1]:
-                    is_sorted = False
-                    break
-
-            if is_sorted:
-                return True
-
-        return False
+        return True
 
 print(Solution().check([3,4,5,1,2]))
 print(Solution().check([2,1,3,4]))
