387140a6bcfc880e37719131e920a65d5bc43968
Neo Zhen Ting <neozt99@gmail.com>
2025-02-10 22:01:38 +0800

    Optimized 447

--- python/447.py
+++ python/447.py
@@ -4,10 +4,13 @@ from typing import List
 
 class Solution:
     def numberOfBoomerangs(self, points: List[List[int]]) -> int:
-        point_to_counter = defaultdict(Counter)
+        result = 0
+        for i in range(len(points)):
+            freq = defaultdict(int)
+            for j in range(len(points)):
+                if i == j:
+                    continue
 
-        for i in range(len(points) - 1):
-            for j in range(i + 1, len(points)):
                 p1 = points[i]
                 p2 = points[j]
 
@@ -15,14 +18,10 @@ class Solution:
                 delta_y = p1[1] - p2[1]
                 distance = delta_x * delta_x + delta_y * delta_y # Omit square root as we are only interested in the relative magnitude
 
-                point_to_counter[(p1[0], p1[1])][distance] += 1
-                point_to_counter[(p2[0], p2[1])][distance] += 1
+                freq[distance] += 1
 
-
-        result = 0
-        for counter in point_to_counter.values():
-            for freq in counter.values():
-                result += freq * (freq - 1)
+            for f in freq.values():
+                result += f * (f - 1)
 
         return result
 
