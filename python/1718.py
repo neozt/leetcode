16004b51cb6fc9e800fb478648b24159972c2fce
Neo Zhen Ting <neozt99@gmail.com>
2025-02-16 11:11:28 +0800

    Optimized 1718

--- python/1718.py
+++ python/1718.py
@@ -6,32 +6,39 @@ class Solution:
         result = [-1] * ((n - 1) * 2 + 1)
         candidates = [i for i in range(n, 0, -1)]
 
-        backtrack(result, candidates, 0, n)
+        def backtrack(index: int) -> bool:
+            if index == len(result):
+                return True
+
+            if result[index] != -1:
+                # Already filled, go next
+                return backtrack(index + 1)
+
+            for candidate in candidates:
+                if candidate == 1:
+                    result[index] = candidate
+                    position = candidates.index(candidate)
+                    candidates.pop(position)
+                    if backtrack(index + 1):
+                        return True
+                    # backtrack
+                    result[index] = -1
+                    candidates.insert(position, candidate)
+                else:
+                    if index + candidate < len(result) and result[index + candidate] == -1:
+                        result[index + candidate] = result[index] = candidate
+                        position = candidates.index(candidate)
+                        candidates.pop(position)
+                        if backtrack(index + 1):
+                            return True
+                        result[index + candidate] = result[index] = -1
+                        candidates.insert(position, candidate)
+
+            return False
+
+        backtrack(0)
 
         return result
-
-def backtrack(result: List[int], candidates: List[int], index: int, n: int) -> bool:
-    if index == len(result):
-        return True
-
-    if result[index] != -1:
-        # Already filled, go next
-        return backtrack(result, candidates, index + 1, n)
-
-    for candidate in candidates:
-        if candidate == 1:
-            result[index] = candidate
-            if backtrack(result, list(filter(lambda x : x != candidate, candidates)), index + 1, n):
-                return True
-            result[index] = -1
-        else:
-            if index + candidate < len(result) and result[index + candidate] == -1:
-                result[index + candidate] = result[index] = candidate
-                if backtrack(result, list(filter(lambda x : x != candidate, candidates)), index + 1, n):
-                    return True
-                result[index + candidate] = result[index] = -1
-
-    return False
 
 print(Solution().constructDistancedSequence(3))
 print(Solution().constructDistancedSequence(5))
