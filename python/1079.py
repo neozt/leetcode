52cd28cb018120d9864f22f9aeb96590d7035f80
Neo Zhen Ting <neozt99@gmail.com>
2025-02-17 21:27:46 +0800

    Cleaned up 1079

--- python/1079.py
+++ python/1079.py
@@ -4,23 +4,22 @@ from collections import Counter
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
         counter = Counter(tiles)
+        sequences = set()
 
-        result = set()
+        populate_sequences('', sequences, counter)
 
-        def backtrack(current: str) -> None:
-            for ch, count in counter.items():
-                if not count:
-                    continue
+        return len(sequences)
 
-                result.add(current + ch)
+def populate_sequences(current: str, sequences, counter) -> None:
+    for ch, count in counter.items():
+        if not count:
+            continue
 
-                counter[ch] -= 1
-                backtrack(current + ch)
-                counter[ch] += 1
+        sequences.add(current + ch)
 
-        backtrack('')
-
-        return len(result)
+        counter[ch] -= 1
+        populate_sequences(current + ch, sequences, counter)
+        counter[ch] += 1
 
 
 print(Solution().numTilePossibilities('AAB'))
