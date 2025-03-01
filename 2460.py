d30813ceaad3a58d8aedd34e6803967aab42be6d
Neo Zhen Ting <neozt99@gmail.com>
2025-03-01 22:48:16 +0800

    Clean up 2460

--- 2460.py
+++ 2460.py
@@ -3,20 +3,27 @@ from typing import List
 
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
-        result = []
-        zeroes = 0
+        result_index = 0
 
         for i in range(len(nums) - 1):
-            if nums[i] == nums[i + 1]:
-                nums[i] *= 2
-                nums[i + 1] = 0
-
             if nums[i] == 0:
-                zeroes += 1
-            else:
-                result.append(nums[i])
+                continue
 
-        return result + [nums[-1]] + [0] * zeroes
+            if nums[i] == nums[i + 1]:
+                nums[result_index] = 2 * nums[i]
+                nums[i + 1] = 0
+            else:
+                nums[result_index] = nums[i]
+            result_index += 1
+
+        nums[result_index] = nums[-1]
+        result_index += 1
+
+        while result_index < len(nums):
+            nums[result_index] = 0
+            result_index += 1
+
+        return nums
 
 
 print(Solution().applyOperations([1,2,2,1,1,0]))
