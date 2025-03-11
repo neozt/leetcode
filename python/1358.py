736ca8d991d3f9487137ed0338b2ee6d87859234
Neo Zhen Ting <neozt99@gmail.com>
2025-03-11 10:19:15 +0800

    Optimized 1358

--- python/1358.py
+++ python/1358.py
@@ -1,37 +1,13 @@
-from collections import deque
-
-
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        a_indices = deque([])
-        b_indices = deque([])
-        c_indices = deque([])
-        indices_map = {
-            'a': a_indices,
-            'b': b_indices,
-            'c': c_indices
-        }
-
-        for i, ch in enumerate(s):
-            arr = indices_map[ch]
-            if arr is not None:
-                arr.append(i)
-
+        previous_index = [-1] * 3
         result = 0
 
         for i in range(len(s)):
-            if a_indices and a_indices[0] < i:
-                a_indices.popleft()
-            if b_indices and b_indices[0] < i:
-                b_indices.popleft()
-            if c_indices and c_indices[0] < i:
-                c_indices.popleft()
-
-            if not a_indices or not b_indices or not c_indices:
-                break
-
-            r = max(a_indices[0], b_indices[0], c_indices[0])
-            result += len(s) - r
+            # Count the number of valid substrings that end at index i by finding the rightmost point (min(previous_index)) that have a, b and c
+            # Every starting point to the left of this rightmost point will form a valid substring
+            previous_index[ord(s[i]) - ord('a')] = i
+            result += 1 + min(previous_index)
 
         return result
 
