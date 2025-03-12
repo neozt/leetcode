385167a0b902a2fe8a659bf666a2b13022750b03
Neo Zhen Ting <neozt99@gmail.com>
2025-03-12 09:43:14 +0800

    Optimized 2529 using binary search

--- python/2529.py
+++ python/2529.py
@@ -1,14 +1,13 @@
+import bisect
 from typing import List
 
 
 class Solution:
     def maximumCount(self, nums: List[int]) -> int:
-        pos = 0
-        neg = 0
-        for num in nums:
-            if num < 0:
-                neg += 1
-            elif num > 0:
-                pos += 1
-
+        neg = bisect.bisect_left(nums, 0)
+        pos = len(nums) - bisect.bisect_right(nums, 0)
         return max(neg, pos)
+
+print(Solution().maximumCount([-2,-1,-1,1,2,3]))
+print(Solution().maximumCount([-3,-2,-1,0,0,1,2]))
+print(Solution().maximumCount([5,20,66,1314]))
