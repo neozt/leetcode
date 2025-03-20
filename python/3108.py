89f31e41aea4a689bef21bf17aaaa0b34627e35c
Neo Zhen Ting <neozt99@gmail.com>
2025-03-20 19:13:48 +0800

    Solved 3108

---
+++ python/3108.py
@@ -0,0 +1,43 @@
+from typing import List
+
+class UnionFind:
+    def __init__(self, n: int):
+        self.parent = [i for i in range(n)]
+
+    def union(self, u: int, v:int):
+        self.parent[self.find(u)] = self.find(v)
+
+
+    def find(self, u: int):
+        while self.parent[u] != self.parent[self.parent[u]]:
+            self.parent[u] = self.parent[self.parent[u]]
+        return self.parent[u]
+
+
+class Solution:
+    def minimumCost(self, n: int, edges: List[List[int]], query: List[List[int]]) -> List[int]:
+        uf = UnionFind(n)
+
+        for u, v, w in edges:
+            uf.union(u, v)
+
+        group_cost = {}
+
+        for u, v, w in edges:
+            parent = uf.find(u)
+            current_cost = group_cost.get(parent, w)
+            new_cost = current_cost & w
+            group_cost[parent] = new_cost
+
+        result = []
+        for u, v in query:
+            if uf.find(u) != uf.find(v):
+                result.append(-1)
+            else:
+                result.append(group_cost.get(uf.find(u)))
+
+        return result
+
+
+print(Solution().minimumCost(5, [[0,1,7],[1,3,7],[1,2,1]], [[0,3],[3,4]]))
+print(Solution().minimumCost(5, [[0,2,7],[0,1,15],[1,2,6],[1,2,1]], [[1,2]]))
