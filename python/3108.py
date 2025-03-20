c0807b59543ba3f326d7e81d5854bc0203d94e0c
Neo Zhen Ting <neozt99@gmail.com>
2025-03-20 19:55:45 +0800

    Optimized 3108

--- python/3108.py
+++ python/3108.py
@@ -3,10 +3,25 @@ from typing import List
 class UnionFind:
     def __init__(self, n: int):
         self.parent = [i for i in range(n)]
+        self.group_cost: List[int | None] = [None] * n
+        self.rank = [1] * n
 
-    def union(self, u: int, v: int):
-        self.parent[self.find(u)] = self.find(v)
+    def union(self, u: int, v: int, w: int):
+        parent_u = self.find(u)
+        parent_v = self.find(v)
+        if self.rank[parent_u] > self.rank[parent_v]:
+            self.rank[parent_u] += self.rank[parent_v]
+        else:
+            self.rank[parent_v] += self.rank[parent_u]
+            parent_u, parent_v = parent_v, parent_u
 
+        self.parent[parent_v] = parent_u
+        cost = w
+        if self.group_cost[parent_u] is not None:
+            cost &= self.group_cost[parent_u]
+        if self.group_cost[parent_v] is not None:
+            cost &= self.group_cost[parent_v]
+        self.group_cost[parent_u] = cost
 
     def find(self, u: int):
         while self.parent[u] != self.parent[self.parent[u]]:
@@ -19,25 +34,19 @@ class Solution:
         uf = UnionFind(n)
 
         for u, v, w in edges:
-            uf.union(u, v)
-
-        group_cost = [None] * n
-
-        for u, v, w in edges:
-            parent = uf.find(u)
-            current_cost = group_cost[parent] if group_cost[parent] is not None else w
-            new_cost = current_cost & w
-            group_cost[parent] = new_cost
+            uf.union(u, v, w)
 
         result = []
         for u, v in query:
-            if uf.find(u) != uf.find(v):
+            parent_u = uf.find(u)
+            if parent_u != uf.find(v):
                 result.append(-1)
             else:
-                result.append(group_cost[uf.find(u)])
+                result.append(uf.group_cost[parent_u])
 
         return result
 
 
 print(Solution().minimumCost(5, [[0,1,7],[1,3,7],[1,2,1]], [[0,3],[3,4]]))
 print(Solution().minimumCost(5, [[0,2,7],[0,1,15],[1,2,6],[1,2,1]], [[1,2]]))
+print(Solution().minimumCost(4, [[2,3,1],[1,3,5],[1,2,6],[3,0,7],[1,3,7],[0,2,5],[0,1,7]], [[2,1],[1,2],[0,1],[2,0],[0,2],[1,2],[3,2],[0,3],[2,1],[1,2]]))
