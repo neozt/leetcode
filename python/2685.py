014d55d617e5caf019735698822a351d2604098c
Neo Zhen Ting <neozt99@gmail.com>
2025-03-22 10:11:17 +0800

    Solved 2685 using DFS

--- python/2685.py
+++ python/2685.py
@@ -2,62 +2,35 @@ from collections import defaultdict
 from typing import List
 
 
-class UnionFind:
-    def __init__(self, n: int):
-        self.parent = [i for i in range(n)]
-        self.rank = [1] * n
-
-    def find(self, u: int):
-        if self.parent[u] == u:
-            return u
-
-        self.parent[u] = self.find(self.parent[u])
-        return self.parent[u]
-
-    def union(self, u: int, v: int):
-        parent_u = self.find(u)
-        parent_v = self.find(v)
-
-        if parent_u == parent_v:
-            return
-
-        if parent_v > parent_u:
-            self.parent[parent_u] = parent_v
-            self.rank[parent_v] += self.rank[parent_u]
-        else :
-            self.parent[parent_v] = parent_u
-            self.rank[parent_u] += self.rank[parent_v]
-
 class Solution:
     def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
-        uf = UnionFind(n)
-
-        # Form connected components
+        adj = defaultdict(list)
         for u, v in edges:
-            uf.union(u, v)
-        components = defaultdict(list)
-        for i in range(n):
-            components[uf.find(i)].append(i)
+            adj[u].append(v)
+            adj[v].append(u)
 
-        # Count how many vertices there are in a component
-        edge_count = defaultdict(int)
-        for u, v in edges:
-            edge_count[uf.find(u)] += 1
-
-        # A component is complete if and only if there are m(m-1)/2 edges between the vertexes
-        # Eg, for a component with m = 1 vertices, we need 0 edges for it to be complete
-        # for m = 2, 0 + 1 edges required
-        # for m = 3, 0 + 1 + 2 edges required
-        # for m = 4, 0 + 1 + 2 + 3 edges required
-        # Therefore, we can use arithmetic sequence formula to calculate the edges required given m vertices.
+        visited = set()
         result = 0
+
+        def dfs(current: int, info: List[int]):
+            visited.add(current)
+            info[0] += 1
+            info[1] += len(adj[current])
+
+            for neighbor in adj[current]:
+                if neighbor not in visited:
+                    dfs(neighbor, info)
+
         for i in range(n):
-            if i != uf.find(i):
-                # We are looping through component, so we only look at the representatives
+            if i in visited:
                 continue
 
-            m = uf.rank[i]
-            if (m * (m - 1) // 2) == edge_count[i]:
+            # node_info[0] = vertices count
+            # node_info[1] = edges count
+            node_info = [0, 0]
+            dfs(i, node_info)
+
+            if node_info[1] == node_info[0] * (node_info[0] - 1):
                 result += 1
 
         return result
