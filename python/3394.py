9f0e63df2872924f14b85af5cf3b2f8e158d9a51
Neo Zhen Ting <neozt99@gmail.com>
2025-03-25 22:48:16 +0800

    Cleaned up 3394

--- python/3394.py
+++ python/3394.py
@@ -18,7 +18,13 @@ class Solution:
 
             return False
 
-        return can_valid_slice(rectangles, lambda x: x[0], lambda x: x[2]) or can_valid_slice(rectangles, lambda x: x[1], lambda x: x[3])
+        # int -> List -> any
+        def array_accessor(i: int):
+            def accessor(arr: List):
+                return arr[i]
+            return accessor
+
+        return can_valid_slice(rectangles, array_accessor(0), array_accessor(2)) or can_valid_slice(rectangles, array_accessor(1), array_accessor(3))
 
 print(Solution().checkValidCuts(5, [[1,0,5,2],[0,2,2,4],[3,2,5,3],[0,4,4,5]]))
 print(Solution().checkValidCuts(4, [[0,0,1,1],[2,0,3,4],[0,2,2,3],[3,0,4,3]]))
