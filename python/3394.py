a33293d5950f26d333f1bc29dfe66dc15bb8b227
Neo Zhen Ting <neozt99@gmail.com>
2025-03-25 22:46:10 +0800

    Cleaned up 3394

--- python/3394.py
+++ python/3394.py
@@ -3,30 +3,22 @@ from typing import List
 
 class Solution:
     def checkValidCuts(self, n: int, rectangles: List[List[int]]) -> bool:
-        sorted_horizontal = sorted(rectangles, key = lambda x: x[0])
-        sorted_vertical = sorted(rectangles, key = lambda x: x[1])
+        def can_valid_slice(rectangles: List[List[int]], extract_start, extract_end):
+            rectangles.sort(key = extract_start)
 
-        current = sorted_horizontal[0][2]
-        cuts = 0
-        for rectangle in sorted_horizontal[1:]:
-            if current <= rectangle[0]:
-                cuts += 1
-                if cuts == 2:
-                    return True
+            current = extract_end(rectangles[0])
+            cuts = 0
+            for rectangle in rectangles[1:]:
+                if current <= extract_start(rectangle):
+                    cuts += 1
+                    if cuts == 2:
+                        return True
 
-            current = max(current, rectangle[2])
+                current = max(current, extract_end(rectangle))
 
-        current = sorted_vertical[0][3]
-        cuts = 0
-        for rectangle in sorted_vertical[1:]:
-            if current <= rectangle[1]:
-                cuts += 1
-                if cuts == 2:
-                    return True
+            return False
 
-            current = max(current, rectangle[3])
-
-        return False
+        return can_valid_slice(rectangles, lambda x: x[0], lambda x: x[2]) or can_valid_slice(rectangles, lambda x: x[1], lambda x: x[3])
 
 print(Solution().checkValidCuts(5, [[1,0,5,2],[0,2,2,4],[3,2,5,3],[0,4,4,5]]))
 print(Solution().checkValidCuts(4, [[0,0,1,1],[2,0,3,4],[0,2,2,3],[3,0,4,3]]))
