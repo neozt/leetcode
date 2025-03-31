d4778929473861b63624c2f2050606fa08463c37
Zhen Ting, Neo <neozt99@gmail.com>
2025-03-31 11:49:05 +0800

    Solved 2551

--- python/2551.py
+++ python/2551.py
@@ -1,29 +1,22 @@
-import math
+import heapq
 from typing import List
-
-# k = 3
-# [1,3,5,1,1,3,5,1]
-
 
 class Solution:
     def putMarbles(self, weights: List[int], k: int) -> int:
-        def helper(start: int, k_left: int) -> tuple[int, int]:
-            if k_left == 1:
-                only_option = weights[start] + weights[-1]
-                return only_option, only_option
+        max_heap = []
+        min_heap = []
+        for i in range(len(weights) - 1):
+            pairwise_sum = weights[i] + weights[i + 1]
+            heapq.heappush(min_heap, pairwise_sum)
+            heapq.heappush(max_heap, -pairwise_sum)
 
-            best_min = math.inf
-            best_max = -math.inf
-            for end in range(start, len(weights) - k_left + 1):
-                curr_min, curr_max = helper(end + 1, k_left - 1)
-                best_min = min(best_min, curr_min + weights[start] + weights[end])
-                best_max = max(best_max, curr_max + weights[start] + weights[end])
+        maximum = minimum = weights[0] + weights[-1]
+        for _ in range(k - 1):
+            minimum += heapq.heappop(min_heap)
+            maximum += -heapq.heappop(max_heap)
 
-            return best_min, best_max
-
-        minimum, maximum = helper(0, k)
         return maximum - minimum
 
-# print(Solution().putMarbles([1,3,5,1], 2))
-# print(Solution().putMarbles([1,3], 2))
+print(Solution().putMarbles([1,3,5,1], 2))
+print(Solution().putMarbles([1,3], 2))
 print(Solution().putMarbles([1,4,2,5,2], 3))
