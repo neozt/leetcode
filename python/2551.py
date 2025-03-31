919f7ab479ae4fa9a627bc3e58d2260feddf8d3c
Zhen Ting, Neo <neozt99@gmail.com>
2025-03-31 11:54:17 +0800

    Optimized 2551

--- python/2551.py
+++ python/2551.py
@@ -1,21 +1,17 @@
-import heapq
 from typing import List
 
 class Solution:
     def putMarbles(self, weights: List[int], k: int) -> int:
-        max_heap = []
-        min_heap = []
+        pairwise_sums = []
         for i in range(len(weights) - 1):
-            pairwise_sum = weights[i] + weights[i + 1]
-            heapq.heappush(min_heap, pairwise_sum)
-            heapq.heappush(max_heap, -pairwise_sum)
+            pairwise_sums.append(weights[i] + weights[i + 1])
+        pairwise_sums.sort()
 
-        maximum = minimum = weights[0] + weights[-1]
-        for _ in range(k - 1):
-            minimum += heapq.heappop(min_heap)
-            maximum += -heapq.heappop(max_heap)
+        difference = 0
+        for i in range(k - 1):
+            difference += pairwise_sums[-(i + 1)] - pairwise_sums[i]
 
-        return maximum - minimum
+        return difference
 
 print(Solution().putMarbles([1,3,5,1], 2))
 print(Solution().putMarbles([1,3], 2))
