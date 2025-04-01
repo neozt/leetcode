b4aa6565a4e8ecd31d5ac3955d03cde56cc1cb7e
Zhen Ting, Neo <neozt99@gmail.com>
2025-04-01 20:29:46 +0800

    Cleaned up 2140

--- python/2140.py
+++ python/2140.py
@@ -7,7 +7,7 @@ class Solution:
     # Space: O(N)
     def mostPoints(self, questions: List[List[int]]) -> int:
         dp = [0] * len(questions)
-        for i in range(len(questions) - 1, -1, -1):
+        for i in reversed(range(len(questions))):
             points, brainpower = questions[i]
             dp[i] = max(
                 dp[i + 1] if i + 1 < len(questions) else 0,
