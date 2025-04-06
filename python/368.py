f7cc412fee259b788ac4e2d53e7b7bf0950dc9dd
Neo Zhen Ting <neozt99@gmail.com>
2025-04-06 10:49:30 +0800

    Optimized 368

--- python/368.py
+++ python/368.py
@@ -7,17 +7,17 @@ class Solution:
 
         dp = [[nums[i]] for i in range(len(nums))]
 
-        for i in range(len(nums) - 2, -1, -1):
+        longest = []
+        for i in range(len(nums) - 1, -1, -1):
             for j in range(i + 1, len(nums)):
                 if nums[j] % nums[i] == 0 and len(dp[j]) + 1 > len(dp[i]):
                     dp[i] = [nums[i]] + dp[j]
 
-        longest = []
-        for l in dp:
-            if len(l) > len(longest):
-                longest = l
+            if len(dp[i]) > len(longest):
+                longest = dp[i]
 
         return longest
 
 print(Solution().largestDivisibleSubset([1,2,3]))
 print(Solution().largestDivisibleSubset([1,2,4,8]))
+print(Solution().largestDivisibleSubset([1]))
