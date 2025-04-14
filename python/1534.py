c862d127555a4d5e0ae4c9a78a325eb0165cb2f5
Neo Zhen Ting <neozt99@gmail.com>
2025-04-14 20:33:49 +0800

    Solved 1534

---
+++ python/1534.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        result = 0
+        for i in range(len(arr) - 2):
+            for j in range(i + 1, len(arr)):
+                for k in range(j + 1, len(arr)):
+                    if abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[k] - arr[i]) <= c:
+                        result += 1
+
+        return result
