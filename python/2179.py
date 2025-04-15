f479f972ca07057cd21bc590eb340d598d030336
Neo Zhen Ting <neozt99@gmail.com>
2025-04-15 21:29:16 +0800

    Attempted 2179

--- python/2179.py
+++ python/2179.py
@@ -5,17 +5,20 @@ class Solution:
     def goodTriplets(self, nums1: List[int], nums2: List[int]) -> int:
         n = len(nums1)
 
-        num_to_pos = {}
+        num_to_pos = [0] * n
         for i, num in enumerate(nums2):
             num_to_pos[num] = i
 
         result = 0
         for i in range(n - 2):
+            x = nums1[i]
             for j in range(i + 1, n - 1):
-                for k in range(j + 1, n):
-                    x, y, z = nums1[i], nums1[j], nums1[k]
-                    if num_to_pos[x] < num_to_pos[y] < num_to_pos[z]:
-                        result += 1
+                y = nums1[j]
+                if num_to_pos[x] < num_to_pos[y]:
+                    for k in range(j + 1, n):
+                        x, y, z = nums1[i], nums1[j], nums1[k]
+                        if num_to_pos[y] < num_to_pos[z]:
+                            result += 1
 
         return result
 
