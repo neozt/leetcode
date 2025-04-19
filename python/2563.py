c268b9b901f8281a1968ebc8865b9336c8b09ce3
Neo Zhen Ting <neozt99@gmail.com>
2025-04-19 11:35:14 +0800

    Optimized 2563

--- python/2563.py
+++ python/2563.py
@@ -5,13 +5,24 @@ from typing import List
 class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
         nums.sort()
+
+        return self.lower_bound(nums, upper + 1) - self.lower_bound(nums, lower)
+
+    def lower_bound(self, nums, value):
+        left = 0
+        right = len(nums) - 1
         result = 0
-        for i in range(len(nums)):
-            left = max(bisect.bisect_left(nums, lower - nums[i]), i + 1)
-            right = max(bisect.bisect_right(nums, upper - nums[i]), i + 1)
-            result += (right - left)
+        while left < right:
+            sum = nums[left] + nums[right]
+            if sum < value:
+                result += right - left
+                left += 1
+            else:
+                right -= 1
 
         return result
+
+
 
 print(Solution().countFairPairs([0,1,7,4,4,5], 3, 6))
 print(Solution().countFairPairs([1,7,9,2,5], 11, 11))
