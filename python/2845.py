5f231a1a60a4a9fd31bff6d940d72153255043cb
Neo Zhen Ting <neozt99@gmail.com>
2025-04-27 11:29:42 +0800

    Fixed 2845

--- python/2845.py
+++ python/2845.py
@@ -4,27 +4,20 @@ from typing import List
 
 class Solution:
     def countInterestingSubarrays(self, nums: List[int], modulo: int, k: int) -> int:
-        prefix = [0] * len(nums)
-
+        result = 0
         current = 0
+        count = Counter({0: 1})
         for i, num in enumerate(nums):
             if num % modulo == k:
                 current += 1
 
-            prefix[i] = current
-
-
-        count = Counter()
-        count[0] = 1
-        result = 0
-        for i, cum in enumerate(prefix):
-            result += count.get((cum - k + modulo) % modulo, 0)
-            if nums[i] % modulo == k:
-                count[cum % modulo] += 1
+            result += count.get((current - k + modulo) % modulo, 0)
+            count[current % modulo] += 1
 
         return result
 
 print(Solution().countInterestingSubarrays([3,2,4],2,1))
 print(Solution().countInterestingSubarrays([3,1,9,6],3, 0))
+print(Solution().countInterestingSubarrays([11,12,21,31],10,1))
 
 
