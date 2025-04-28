64f871acc61775d6648f0a155ed970d2c89434c6
Neo Zhen Ting <neozt99@gmail.com>
2025-04-28 23:13:09 +0800

    Optimized 2302

--- python/2302.py
+++ python/2302.py
@@ -4,29 +4,15 @@ from typing import List
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
         result = 0
+        total = 0
+        i = 0
+        for j in range(len(nums)):
+            total += nums[j]
+            while i <= j and total * (j - i + 1) >= k:
+                total -= nums[i]
+                i += 1
 
-        prefix = [0]
-        acc = 0
-        for num in nums:
-            acc += num
-            prefix.append(acc)
-
-        for i in range(len(nums)):
-            if nums[i] >= k:
-                continue
-
-            left = i
-            right = len(nums) - 1
-            while left < right:
-                middle = (left + right + 1) // 2
-                score = (prefix[middle + 1] - prefix[i]) * (middle - i + 1)
-
-                if score >= k:
-                    right = middle - 1
-                else:
-                    left = middle
-
-            result += left - i + 1
+            result += j - i + 1
 
         return result
 
