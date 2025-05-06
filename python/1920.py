6b048f745f31bdc9ca523af0e69512ccea4a8d9d
Neo Zhen Ting <neozt99@gmail.com>
2025-05-06 21:19:55 +0800

    Solved 1920

---
+++ python/1920.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in range(len(nums)):
+            ans.append(nums[nums[i]])
+
+        return ans
+
+
+print(Solution().buildArray([0,2,1,5,3,4]))
+print(Solution().buildArray([5,0,1,2,3,4]))
