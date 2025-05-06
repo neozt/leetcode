f925cb7942bdabfa01d79adb7cde997093376dde
Neo Zhen Ting <neozt99@gmail.com>
2025-05-06 21:25:57 +0800

    Solved 1920 in place

--- python/1920.py
+++ python/1920.py
@@ -3,12 +3,13 @@ from typing import List
 
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        ans = []
         for i in range(len(nums)):
-            ans.append(nums[nums[i]])
+            nums[i] = nums[i] + (nums[nums[i]] % 1000) * 1000
 
-        return ans
+        for i in range(len(nums)):
+            nums[i] = nums[i] // 1000
 
+        return nums
 
 print(Solution().buildArray([0,2,1,5,3,4]))
 print(Solution().buildArray([5,0,1,2,3,4]))
