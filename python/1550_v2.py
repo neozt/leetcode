cb7824d5ffc71e04b532828574adadff7d9845ba
Neo Zhen Ting <neozt99@gmail.com>
2025-05-11 19:40:51 +0800

    Solved 1550

--- python/1550_v2.py
+++ python/1550_v2.py
@@ -3,14 +3,14 @@ from typing import List
 
 class Solution:
     def threeConsecutiveOdds(self, arr: List[int]) -> bool:
-        odd_count = 0
+        consecutive_odds = 0
         for num in arr:
             if num % 2 != 0:
-                odd_count += 1
+                consecutive_odds += 1
             else:
-                odd_count = 0
+                consecutive_odds = 0
 
-            if odd_count >= 3:
+            if consecutive_odds >= 3:
                 return True
 
         return False
